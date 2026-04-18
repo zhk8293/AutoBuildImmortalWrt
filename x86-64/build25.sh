173d21b096800e4e99013e64a561e03de80a86ad
zhk8293 <53077919+zhk8293@users.noreply.github.com>
2026-04-18 19:40:00 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -28,6 +28,7 @@ else
   # 同步第三方软件仓库run/apk
   echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
   git clone --depth=1 https://github.com/wukongdaily/apk.git /tmp/store-apk-repo
+  git clone --depth=1 https://github.com/zhk8293/apk.git /tmp/store-apk-repo
 
   # 拷贝 run/x86 下所有 run 文件和apk文件 到 extra-packages 目录
   mkdir -p /home/build/immortalwrt/extra-packages
